c48f664cba2fb9e1be249e012318345027d28188
sanjeet1999 <sanjeetkr987152@gmail.com>
2025-06-15 19:04:19 +0530

    "adding env properly"

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT;
 app.use(express.json())
 
 app.use(cors({
-  origin: `http://localhost:5173`,
+  origin: process.env.origin || "http://localhost:5173",
   credentials: true,
 }));
 // app.use(cors());
